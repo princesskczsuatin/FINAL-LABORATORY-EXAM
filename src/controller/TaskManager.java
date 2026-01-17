a900e601060d64b116997bfa4f0f55af0ca51cde
princesskczsuatin <princesskczsuatin@gmail.com>
2026-01-17 09:16:25 +0800

    Update TaskManager.java

--- src/controller/TaskManager.java
+++ src/controller/TaskManager.java
@@ -2,6 +2,7 @@ package controller;
 import model.Task;
 import java.util.ArrayList;
 public class TaskManager {
+
     private ArrayList<Task> tasks;
     public TaskManager() {
         tasks = new ArrayList<>();
@@ -11,6 +12,16 @@ public class TaskManager {
     }
     public ArrayList<Task> getTasks() {
         return tasks;
+    }
+    public void updateTask(int index, Task updatedTask) {
+        if (index >= 0 && index < tasks.size()) {
+            tasks.set(index, updatedTask);
+        }
+    }
+    public void removeTask(int index) {
+        if (index >= 0 && index < tasks.size()) {
+            tasks.remove(index);
+        }
     }
     public int generateTaskId() {
         return tasks.size() + 1;
