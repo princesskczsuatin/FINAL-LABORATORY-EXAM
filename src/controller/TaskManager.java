f72984f4cf8e7d0c20fac9338e4d3d27c433c5b8
princesskczsuatin <princesskczsuatin@gmail.com>
2026-01-17 08:54:34 +0800

    Update TaskManager.java

--- src/controller/TaskManager.java
+++ src/controller/TaskManager.java
@@ -1,1 +1,18 @@
-
+package controller;
+import model.Task;
+import java.util.ArrayList;
+public class TaskManager {
+    private ArrayList<Task> tasks;
+    public TaskManager() {
+        tasks = new ArrayList<>();
+    }
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+    public ArrayList<Task> getTasks() {
+        return tasks;
+    }
+    public int generateTaskId() {
+        return tasks.size() + 1;
+    }
+}
